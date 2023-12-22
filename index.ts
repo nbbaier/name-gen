094d2b73c3efe845fe019e3f6597802ee1221dd9
Nico Baier <nico.baier@gmail.com>
2023-12-22 10:16:27 -0600

    Update project name generation logic

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { nouns, adjectives } from "./words.js";
 
+// const flag = Bun.argv.slice(1) || "no opt";
+
 const random = (...arr: any[]) => {
   arr = arr.flat(1);
   return arr[Math.floor(arr.length * Math.random())];
@@ -11,9 +13,19 @@ export function generateProjectName() {
   if (validNouns.length < 2) {
     return `${random(adjectives)}-${random(nouns)}`;
   }
-  const noun = random(validNouns);
-  return `${adjective}-${noun}`;
+
+  const noun: string = random(validNouns);
+  switch (Bun.argv.slice(2)[0]) {
+    case "u":
+      return `${adjective}_${noun}`;
+    case "c":
+      return `${adjective}${noun.charAt(0).toUpperCase() + noun.slice(1)}`;
+    default:
+      return `${adjective}-${noun}`;
+  }
 }
 
 let name = generateProjectName();
 console.log(name);
+
+console.log();
